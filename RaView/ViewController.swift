3693fc4997cfbcd5b453f51cff74396ad4ba2b1b
Hayato <hayabit12@gmail.com>
2019-02-21 20:10:54 +0900

    Initial Commit

---
+++ RaView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RaView
+//
+//  Created by 南山駿人 on 2019/02/21.
+//  Copyright © 2019 midbit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
