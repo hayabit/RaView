d7c76a107a937a8ccf00655a6db2c9e77037cb64
Hayato <hayabit12@gmail.com>
2019-03-27 02:20:09 +0900

    Implement storage function

--- RaView/PostViewController.swift
+++ RaView/PostViewController.swift
@@ -41,7 +41,11 @@ class PostViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     var textView = UITextView()
     // main image
     var selectImage = UIImageView()
-
+    // target image URL
+    var imageURL = NSURL()
+    
+    var timeStamp = String()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -110,7 +114,92 @@ class PostViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     
     // MARK: - Storage
     func saveSelectedImageToStorage(){
+        // ストレージに保存する対象の画像を定義
+        var targetImageURL = NSURL()
+        targetImageURL = imageURL
         
+        timeStamp = createTimestamp()
+        
+        uploadImage(targetImageURL: targetImageURL)
+
+    }
+    
+    // MARK: - create timestamp
+    func createTimestamp() -> String{
+        let timeInterval = NSDate().timeIntervalSince1970
+        let myTimeInterval = TimeInterval(timeInterval)
+        let time = NSDate(timeIntervalSince1970: TimeInterval(myTimeInterval))
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyyMMddHHmmss"
+        let timestamp = formatter.string(from: time as Date)
+        return timestamp
+    }
+    
+    // MARK: - image upload to storage
+    func uploadImage(targetImageURL: NSURL){
+        
+        let storage = Storage.storage()
+        
+        let storageRef = storage.reference()
+        
+        
+        
+        let localFile = URL(string: "\(targetImageURL)")!
+        
+        
+        let imageRef = storageRef.child("images/\(timeStamp).jpg")
+        
+        // Create the file metadata
+        let metadata = StorageMetadata()
+        metadata.contentType = "image/jpeg"
+        
+        // Upload file and metadata to the object 'images/mountains.jpg'
+        let uploadTask = imageRef.putFile(from: localFile, metadata: metadata)
+        
+        // Listen for state changes, errors, and completion of the upload.
+        uploadTask.observe(.resume) { snapshot in
+            // Upload resumed, also fires when the upload starts
+        }
+        
+        uploadTask.observe(.pause) { snapshot in
+            // Upload paused
+        }
+        
+        uploadTask.observe(.progress) { snapshot in
+            // Upload reported progress
+            let percentComplete = 100.0 * Double(snapshot.progress!.completedUnitCount)
+                / Double(snapshot.progress!.totalUnitCount)
+            print("\(percentComplete)")
+        }
+        
+        uploadTask.observe(.success) { snapshot in
+            // Upload completed successfully
+        }
+        
+        uploadTask.observe(.failure) { snapshot in
+            if let error = snapshot.error as NSError? {
+                switch (StorageErrorCode(rawValue: error.code)!) {
+                case .objectNotFound:
+                    // File doesn't exist
+                    break
+                case .unauthorized:
+                    // User doesn't have permission to access file
+                    break
+                case .cancelled:
+                    // User canceled the upload
+                    break
+                    
+                    /* ... */
+                    
+                case .unknown:
+                    // Unknown error occurred, inspect the server response
+                    break
+                default:
+                    // A separate error occurred. This is a good place to retry the upload.
+                    break
+                }
+            }
+        }
     }
 
     //  MARK: - Database
@@ -121,10 +210,12 @@ class PostViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         // Add a new document with a generated ID
         var ref: DocumentReference? = nil
         
-        ref = db.collection("users").addDocument(data: [
-            "user": "sample",
-            "submissions": [
-                "url": "https://sample.com/sample.mov",
+        saveSelectedImageToStorage()
+        
+        ref = db.collection("user").addDocument(data: [
+            "userId": "sample",
+            "submission": [
+                "url": "images/\(timeStamp).jpg",
                 "caption": textView.text,
                 "hashtags": textView.text.hashtags(),
                 "likes": 0,
@@ -167,8 +258,12 @@ class PostViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         let img = info[.originalImage] as! UIImage
         print(img as Any)
         
+        let url = info[.imageURL] as! NSURL
+        
         selectImage.image = img
         selectImage.contentMode = .scaleAspectFit
+        
+        imageURL = url
         
         imagePicker.dismiss(animated: true, completion: nil)
     }
